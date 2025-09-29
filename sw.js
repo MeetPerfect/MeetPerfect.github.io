0dbacfff396a564d96c7f4634983d38bcbdb191d
gongkaiming <12919034+gongkaiming@user.noreply.gitee.com>
2025-09-29 11:35:47 +0800

    Site updated: 2025-09-29 11:35:45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759116890641";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759116943610";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
